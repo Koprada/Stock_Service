f2cebaf21beee1edda2060de2f4be9d7470af0e5
Kevyn Prada <koprada@javeriana.edu.co>
2024-08-15 11:15:01 -0500

    create package model

--- stock-service/src/main/java/com/emazon/stock_service/Domain/model/Categoria.java
+++ stock-service/src/main/java/com/emazon/stock_service/Domain/model/Categoria.java
@@ -1,6 +1,10 @@
 package com.emazon.stock_service.Domain.model;
 
+import jakarta.persistence.*;
+import java.util.List;
 import lombok.*;
+
+@Entity
 
 @Getter
 @Setter
@@ -8,7 +12,14 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nombre;
     private String descripcion;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Articulo> articulos;
 }
